b534137401a35a3b8dab730b46035544301cf33a
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-08-14 17:43:45 -0300

    feat: :sparkles: 2760 - Entrada e Saída de String

---
+++ Iniciante/2760.py
@@ -0,0 +1,9 @@
+frase1 = input()
+frase2 = input()
+frase3 = input()
+
+print(f"{frase1}{frase2}{frase3}")
+print(f"{frase2}{frase3}{frase1}")
+print(f"{frase3}{frase1}{frase2}")
+
+print(f"{frase1[0:10]}{frase2[0:10]}{frase3[0:10]}")
