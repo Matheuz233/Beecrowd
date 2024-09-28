21c530b8dadf6923b1af27ab57ea6ba40a406497
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-09-28 17:00:00 -0300

    feat :sparkles: 1546 - Feedback

---
+++ Ad-Hoc/1546.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    for _ in range(int(input())):
+        k = int(input())
+
+        if k == 1:
+            print("Rolien")
+        if k == 2:
+            print("Naej")
+        if k == 3:
+            print("Elehcim")
+        if k == 4:
+            print("Odranoel")
