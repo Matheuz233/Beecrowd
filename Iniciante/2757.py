7ef5bb88a57c123a5f98387110adacfcb88a0be6
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-10-12 17:00:00 -0300

    feat :sparkles: 2757 - Entrada e Saída de Números Inteiros

---
+++ Iniciante/2757.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+print('A = %d, B = %d, C = %d' % (a, b, c))
+print('A = %10.0d, B = %10d, C = %10d' % (a, b, c))
+print('A = %s, B = %s, C = %s' %
+      (str(a).zfill(10), str(b).zfill(10), str(c).zfill(10)))
+print('A = {:<10}, B = {:<10}, C = {:<10}'.format(a, b, c))
