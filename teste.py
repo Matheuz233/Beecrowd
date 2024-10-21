d60d39a091ce2258a7c0ac91f2116ce861c58983
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-10-20 23:55:18 -0300

    feat: :sparkles: 2759 - Entrada e Saída de Carácter

---
+++ teste.py
@@ -0,0 +1,7 @@
+a = input()
+b = input()
+c = input()
+
+print("A = " + a + ", B = " + b + ", C = " + c)
+print("A = " + b + ", B = " + c + ", C = " + a)
+print("A = " + c + ", B = " + a + ", C = " + b)
