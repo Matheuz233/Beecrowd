ae7e53316ab36adb0b242c53fa6bfe1db1d4f415
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2025-01-19 21:41:39 -0300

    feat: :sparkles: 1940 - Jogo da Estratégia

---
+++ Ad-Hoc/1940.py
@@ -0,0 +1,19 @@
+j, r = map(int, input().split())
+
+pontos = list(map(int, input().split()))
+
+resultado = [0] * j
+
+for i in range(0, len(pontos), j):
+  rodada = pontos[i:j+i]
+  for k in range(j):
+    resultado[k] += rodada[k]
+
+maiorValor = max(resultado)
+
+ultimoIndex = -1
+for i in range(len(resultado)):
+    if resultado[i] == maiorValor:
+        ultimoIndex = i
+
+print(ultimoIndex + 1) 
