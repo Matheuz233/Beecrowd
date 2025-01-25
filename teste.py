6376fb204b406d240d45018cd786ca7c0a55ae4b
matheuz233 <matheusaugus233@gmail.com>
2025-01-25 17:00:00 -0300

    feat: :fire: Remocao de Arquivo Teste.py

--- teste.py
+++
@@ -1,7 +0,0 @@
-a = input()
-b = input()
-c = input()
-
-print("A = " + a + ", B = " + b + ", C = " + c)
-print("A = " + b + ", B = " + c + ", C = " + a)
-print("A = " + c + ", B = " + a + ", C = " + b)
